e238570088ab90cf3abef3f200d66d672285f05b
Isabellukk <isabellukk@gmail.com>
2021-10-04 19:58:34 -0700

    Added styling and fixed the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,8 @@ import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
 
 const NavBar = () => {
   return(
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <div className="navy">
+    <Navbar collapseOnSelect expand="lg left" bg="dark" variant="dark">
       <Container>
       <Navbar.Brand href="/bottles">Liquorganizer</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -13,21 +14,22 @@ const NavBar = () => {
       <Navbar.Collapse id="responsive-navbar-nav collasible-nav-dropdown">
         <Nav className="me-auto">
           <NavDropdown title="More" id="">
-            <NavDropdown.Item href="/bottles/new">Add Bottle</NavDropdown.Item>
+            <NavDropdown.Item href="/bottles/new">Add A Bottle</NavDropdown.Item>
 
             <NavDropdown.Item href="#action/3.2">Another Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.3">Another Action</NavDropdown.Item>
           </NavDropdown>
         </Nav>
         <Nav>
-          <Nav.Link href="#deets">Login</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link eventKey={2} href="/logout">
             Log Out
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
       </Container>
     </Navbar>
+    </div>
   )}
 
 

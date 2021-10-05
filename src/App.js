7a3b769fd0f35485c6ba8cb61594636ef5f00cee
Jay Thurber <jthurber87@gmail.com>
2021-10-05 15:49:11 -0600

    pre pull

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import ItemDetail from './components/ItemDetail';
 import ItemLists from './components/ItemLists';
 import NewForm from './components/NewForm';
 import EditForm from './components/EditForm';
@@ -16,7 +15,6 @@ function App() {
           <Route exact path="/bottles" component={ItemLists} />
           <Route exact path="/bottles/new" render={(routerProps)=><NewForm {...routerProps}/>}/>
           <Route exact path="/bottles/:id/edit" component={EditForm} />
-          <Route exact path="/bottles/:id" component={ItemDetail} />
         </Switch>
       </Router>
     </div>

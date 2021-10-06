7840f40b607a71a55f56523c690a93a355a341f7
Dae Young Hwang <daeh@Daes-Air.attlocal.net>
2021-10-06 12:27:41 -0700

    blah blah

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,17 +5,11 @@ const Login = () => {
         <div>
             <h1>Login Page</h1>
             <form>
-
                 <label htmlFor="username">User Name</label>
-
                 <input type="text" name="username" id="username" />
-
-
                 <label htmlFor="password">Password</label>
-
                 <input type="password" name="password" id="password" />
                 <input type="submit" value="Login" />
-
             </form>
         </div>
     </>)
